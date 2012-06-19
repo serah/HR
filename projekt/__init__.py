232c0357e7baf09702134ed8a94312598eb81c09
Pronoy Chopra <pronoyc@gmail.com>
2012-06-19 18:44:48 +0530

    SQLAlchemy updated

--- projekt/__init__.py
+++ projekt/__init__.py
@@ -1,13 +1,13 @@
 #imports
 import pymongo
 from pymongo import Connection
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
-from flaskext.wtf import Form, TextField, TextAreaField, \
+from flask.ext.wtf import Form, TextField, TextAreaField, \
     PasswordField, SubmitField, Required, ValidationError
-from flaskext.bcrypt import Bcrypt, generate_password_hash, \
+from flask.ext.bcrypt import Bcrypt, generate_password_hash, \
 	check_password_hash
-	
 
 #define app
 app = Flask(__name__)
