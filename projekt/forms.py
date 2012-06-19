45431a731ad302f3a6a6499d0de05bb63384672f
Pronoy Chopra <pronoyc@gmail.com>
2012-06-19 14:01:10 +0530

    forms created

---
+++ projekt/forms.py
@@ -0,0 +1,6 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+from flaskext.wtf import Form, TextField, TextAreaField, \
+    PasswordField, SubmitField, Required, ValidationError
+from flaskext.bcrypt import Bcrypt, generate_password_hash, \
+	check_password_hash
