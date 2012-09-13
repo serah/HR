35b002f2789588a8c3386e15e7ca59a1bf2dc797
serah <serah4291@gmail.com>
2012-09-13 12:35:59 +0530

    removed projekt

--- projekt/forms.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
-from flask.ext.wtf import Form, TextField, TextAreaField, \
-    PasswordField, SubmitField, Required, ValidationError
-from flaskext.bcrypt import Bcrypt, generate_password_hash, \
-	check_password_hash
